bfe023e2d1b7ef25ffc1ef28c82198e3dd215fef
Elvis Jr Crego <elvis.crego@gmail.com>
2019-04-23 22:47:23 -0400

    mostrando menu dependiendo del rol

---
+++ GestionBares/Utils/DefinicionRoles.cs
@@ -0,0 +1,8 @@
+namespace GestionBares.Utils
+{
+    public static class DefinicionRoles
+    {
+        public const string Administrador = "ADMINISTRADOR";
+        public const string Dependiente = "DEPENDIENTE";
+    }
+}

1d835e1f47cc0e653cc81d4c50a42d335f3603fd
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-06 14:35:25 -0400

    Cambios en modelos de gestion de ventas

---
+++ GestionBares/Models/TrasladoVenta.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionBares.Models
+{
+    public class TrasladoVenta
+    {
+        public int Id { get; set; }
+        public int TurnoId { get; set; }
+        public virtual Turno Turno { get; set; }
+        public DateTime Fecha { get; set; }
+
+        [Display(Name = "Producto")]
+        public int ProductoId { get; set; }
+        public virtual Producto Producto { get; set; }
+        public double Cantidad { get; set; }
+
+        [Display(Name = "Destino")]
+        public int DestinoId { get; set; }
+        public virtual Bar Destino { get; set; }
+
+        public string UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}

3e8845e158ecd5ed7328b18c9dd08fd38ff52c2a
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-10 14:37:33 -0400

    agregando view modelos para mostrar solicitudes

---
+++ GestionBares/ViewModels/SolicitudBarVM.cs
@@ -0,0 +1,9 @@
+namespace GestionBares.ViewModels
+{
+    public class SolicitudBarVM
+    {
+        public int BarId { get; set; }
+        public string Bar { get; set; }
+        public double Cantidad { get; set; }
+    }
+}

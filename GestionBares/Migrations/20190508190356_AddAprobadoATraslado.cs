fd3b34e79709601645d0c6042ea9890a59528100
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-09 23:00:18 -0400

    adding aprobado a traslado

---
+++ GestionBares/Migrations/20190508190356_AddAprobadoATraslado.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestionBares.Migrations
+{
+    public partial class AddAprobadoATraslado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Aprobado",
+                table: "Traslados",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "7e05f4be-aed9-4e38-aeb3-a3f720c093e1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "ec9c4da8-31a5-48cd-a683-6ecd547a4edc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "1e624af6-988e-4f66-ad00-e841d22156df");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4",
+                column: "ConcurrencyStamp",
+                value: "a8962e98-4b6e-4bb4-8551-e713f8b7a888");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Aprobado",
+                table: "Traslados");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "8d5bb64b-c8c5-48c2-9ffa-870bf7af4508");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "14dc3d7b-19c9-4206-aefc-e540695e0a12");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "9344561d-dc18-4294-b251-88a248fe4777");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4",
+                column: "ConcurrencyStamp",
+                value: "17f2c370-7010-4bf5-9c47-477842366e19");
+        }
+    }
+}

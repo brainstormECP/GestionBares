7b3057170a2f5c4743f5cd4498f5f178f39e4a5f
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-10 21:10:57 -0400

    modificando como se muestran las solicitudes y cargando existencias de almacen

--- GestionBares/ViewModels/SolicitudVM.cs
+++ GestionBares/ViewModels/SolicitudVM.cs
@@ -4,13 +4,12 @@ namespace GestionBares.ViewModels
 {
     public class SolicitudVM
     {
+        public int Id { get; set; }
         public int ProductoId { get; set; }
         public string Producto { get; set; }
-        public ICollection<SolicitudBarVM> Bares { get; set; }
+        public int BarId { get; set; }
+        public string Bar { get; set; }
+        public double Cantidad { get; set; }
         public double EnAlmacen { get; set; }
-        public SolicitudVM()
-        {
-            Bares = new List<SolicitudBarVM>();
-        }
     }
 }

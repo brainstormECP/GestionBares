4df34b21e1d0dd12acb0307b316e670fd88cb7f3
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-10 20:52:22 -0400

    agregando bd de existenciass de almacen

---
+++ GestionBares/Models/AlmacenModels/Existencia.cs
@@ -0,0 +1,9 @@
+namespace GestionBares.Models.AlmacenModels
+{
+    public class Existencia
+    {
+        public int Id {get;set;}
+        public string CodigoProducto { get; set; }
+        public double Cantidad { get; set; }
+    }
+}

7d1bbd7f39ca014d238a4144b3e10c9dd4a83724
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-13 11:57:43 -0400

    cambios para asignacion

--- GestionBares/Models/Standard.cs
+++ GestionBares/Models/Standard.cs
@@ -1,4 +1,4 @@
-﻿using GestionBares.ViewModel;
+﻿using GestionBares.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,14 +10,16 @@ namespace GestionBares.Models
 {
     public class Standard
     {
-        public int Id { get; set; }        
+        public int Id { get; set; }
         public int BarId { get; set; }
         public Bar Bar { get; set; }
-        public int ProductoId {get;set;}
+        public int ProductoId { get; set; }
         public Producto Producto { get; set; }
-        [BindProperty][NotMapped]
+        [BindProperty]
+        [NotMapped]
         public List<int> SelectProductosId { get; set; }
-        [BindProperty][NotMapped]
+        [BindProperty]
+        [NotMapped]
         public List<CheckedProductos> CheckedProductos { get; set; }
 
         public Standard()

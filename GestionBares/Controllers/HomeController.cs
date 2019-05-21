6ecddce009a14f51318a6936430e6a78eafbe696
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-20 22:08:50 -0400

    mostrando turno en home de dependiente

--- GestionBares/Controllers/HomeController.cs
+++ GestionBares/Controllers/HomeController.cs
@@ -6,14 +6,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GestionBares.Models;
 using Microsoft.AspNetCore.Authorization;
+using GestionBares.Data;
+using Microsoft.EntityFrameworkCore;
+using GestionBares.Utils;
 
 namespace GestionBares.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
         public IActionResult Index()
         {
+            if (User.IsInRole(DefinicionRoles.Dependiente))
+            {
+                var dependiente = _db.Set<Dependiente>().SingleOrDefault(u => u.Usuario.UserName == User.Identity.Name);
+                if (!_db.Set<Turno>().Any(t => t.DependienteId == dependiente.Id && t.Activo))
+                {
+                    ViewBag.Turno = "No existe un turno iniciado, inicie uno para comenzar a trabajar.";
+                }
+                else
+                {
+                    var turno = _db.Set<Turno>()
+                        .Include(t => t.Bar)
+                        .SingleOrDefault(t => t.DependienteId == dependiente.Id && t.Activo);
+                    ViewBag.Turno = $"Usted tiene un turno abierto en el bar {turno.Bar.Nombre} con fecha {turno.FechaInicio.ToShortDateString()}.";
+                }
+            }
             return View();
         }
 

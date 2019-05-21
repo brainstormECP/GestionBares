a04014fd8454e14431a14743916a0f0f0b0cf4e3
Elvis Jr Crego <elvis.crego@gmail.com>
2019-05-21 07:45:44 -0400

    sugerencia de pedidos

---
+++ GestionBares/Controllers/SugerenciasPedidosController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestionBares.Data;
+using GestionBares.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using GestionBares.Utils;
+
+namespace GestionBares.Controllers
+{
+    [Authorize]
+    public class SugerenciasPedidosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly SugerenciasPedidos _sugerencias;
+
+        public SugerenciasPedidosController(ApplicationDbContext context)
+        {
+            _context = context;
+            _sugerencias = new SugerenciasPedidos(context);
+        }
+
+        // GET: Traslados
+        public IActionResult Index()
+        {
+            if (!User.IsInRole(DefinicionRoles.Dependiente))
+            {
+                return View();
+            }
+            var dependiente = _context.Set<Dependiente>().SingleOrDefault(u => u.Usuario.UserName == User.Identity.Name);
+            if (!_context.Set<Turno>().Any(t => t.DependienteId == dependiente.Id && t.Activo))
+            {
+                return RedirectToAction("Nuevo", "Turnos");
+            }
+            var turno = _context.Set<Turno>()
+                .Include(t => t.Bar)
+                .SingleOrDefault(t => t.DependienteId == dependiente.Id && t.Activo);
+            var sugerencias = _sugerencias.Sugerencias(turno.Id);
+            return View(sugerencias);
+        }
+
+        public IActionResult Ventas()
+        {
+            if (!User.IsInRole(DefinicionRoles.Dependiente))
+            {
+                return View();
+            }
+            var dependiente = _context.Set<Dependiente>().SingleOrDefault(u => u.Usuario.UserName == User.Identity.Name);
+            if (!_context.Set<Turno>().Any(t => t.DependienteId == dependiente.Id && t.Activo))
+            {
+                return RedirectToAction("Nuevo", "Turnos");
+            }
+            var turno = _context.Set<Turno>()
+                .Include(t => t.Bar)
+                .SingleOrDefault(t => t.DependienteId == dependiente.Id && t.Activo);
+            var sugerencias = _sugerencias.SugerenciasVentas(turno.Id);
+            return View(sugerencias);
+        }
+    }
+}
---
+++ GestionBares/Utils/SugenrenciasPedidos.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GestionBares.Data;
+using GestionBares.Models;
+
+namespace GestionBares.Utils
+{
+    class Promedio
+    {
+        public int ProductoId { get; set; }
+        public double Cantidad { get; set; }
+        public int Cuenta { get; set; }
+
+    }
+    public class SugerenciasPedidos
+    {
+        private ApplicationDbContext _db;
+        private ExistenciasService _existenciaService;
+
+        public SugerenciasPedidos(ApplicationDbContext context)
+        {
+            _db = context;
+            _existenciaService = new ExistenciasService(context);
+        }
+
+        public List<DetalleControlExistencia> Sugerencias(int turnoId)
+        {
+            var result = new List<DetalleControlExistencia>();
+            var turno = _db.Set<Turno>().FirstOrDefault(t => t.Id == turnoId);
+            var turnosAnteriores = _db.Set<Turno>()
+                .Where(t => t.FechaInicio < turno.FechaInicio && t.FechaInicio > turno.FechaInicio.AddMonths(-6) && t.BarId == turno.BarId)
+                .ToList();
+            var data = new List<Promedio>();
+            foreach (var item in turnosAnteriores)
+            {
+                var existencias = _existenciaService.ExistenciaDeBarPorTurno(item.Id);
+                foreach (var e in existencias)
+                {
+                    if (data.Any(d => d.ProductoId == e.ProductoId))
+                    {
+                        var temp = data.SingleOrDefault(d => d.ProductoId == e.ProductoId);
+                        temp.Cantidad += e.Consumo;
+                        temp.Cuenta++;
+                    }
+                    else
+                    {
+                        data.Add(new Promedio
+                        {
+                            ProductoId = e.ProductoId,
+                            Cantidad = e.Consumo,
+                            Cuenta = 1,
+                        });
+                    }
+                }
+            }
+            var existenciasAnteriores = _existenciaService.ExistenciaAnterior(turno.BarId, 0, DateTime.Now);
+            foreach (var item in existenciasAnteriores)
+            {
+                var temp = data.SingleOrDefault(d => d.ProductoId == item.ProductoId);
+                if (temp != null)
+                {
+                    var diferencia = item.Cantidad - temp.Cantidad / temp.Cuenta;
+                    if (diferencia < 0)
+                    {
+                        result.Add(new DetalleControlExistencia() { ProductoId = item.ProductoId, Producto = _db.Set<Producto>().Find(item.ProductoId), Cantidad = Math.Abs(diferencia) });
+                    }
+                }
+
+            }
+            return result;
+        }
+        public List<DetalleControlExistencia> SugerenciasVentas(int turnoId)
+        {
+            var result = new List<DetalleControlExistencia>();
+            var turno = _db.Set<Turno>().FirstOrDefault(t => t.Id == turnoId);
+            var turnosAnteriores = _db.Set<Turno>()
+                .Where(t => t.FechaInicio < turno.FechaInicio && t.FechaInicio > turno.FechaInicio.AddMonths(-6) && t.BarId == turno.BarId).ToList();
+            var data = new List<Promedio>();
+            foreach (var item in turnosAnteriores)
+            {
+                var existencias = _existenciaService.ExistenciaVentaDeBarPorTurno(item.Id);
+                foreach (var e in existencias)
+                {
+                    if (data.Any(d => d.ProductoId == e.ProductoId))
+                    {
+                        var temp = data.SingleOrDefault(d => d.ProductoId == e.ProductoId);
+                        temp.Cantidad += e.Consumo;
+                        temp.Cuenta++;
+                    }
+                    else
+                    {
+                        data.Add(new Promedio
+                        {
+                            ProductoId = e.ProductoId,
+                            Cantidad = e.Consumo,
+                            Cuenta = 1,
+                        });
+                    }
+                }
+            }
+            var existenciasAnteriores = _existenciaService.ExistenciaVentaAnterior(turno.BarId, 0, DateTime.Now);
+            foreach (var item in existenciasAnteriores)
+            {
+                var temp = data.SingleOrDefault(d => d.ProductoId == item.ProductoId);
+                if (temp != null)
+                {
+                    var diferencia = item.Cantidad - temp.Cantidad / temp.Cuenta;
+                    if (diferencia < 0)
+                    {
+                        result.Add(new DetalleControlExistencia() { ProductoId = item.ProductoId, Producto = _db.Set<Producto>().Find(item.ProductoId), Cantidad = Math.Abs(diferencia) });
+                    }
+                }
+
+            }
+            return result;
+        }
+    }
+}

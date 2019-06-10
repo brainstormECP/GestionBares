c8df0b407a0ceca88b7e3deb86e5754a84689b40
gitName <a@b.c>
2019-06-10 13:48:59 -0400

    Agregando notificaciones

---
+++ GestionBares/ViewComponents/NotificacionesViewComponent.cs
@@ -0,0 +1,61 @@
+﻿using GestionBares.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionBares.Models;
+using GestionBares.ViewModels;
+using GestionBares.Utils;
+
+namespace GestionBares.ViewComponents
+{
+
+    public class NotificacionesViewComponent : ViewComponent
+    {
+        private readonly DbContext _db;
+
+        public NotificacionesViewComponent(DbContext context)
+        {
+            _db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificaciones = new List<NotificacionVM>();
+
+            var traslados = _db.Set<Traslado>()
+                .Include(t => t.Destino)
+                .Include(t => t.Turno.Bar)
+                .Include(t => t.Turno.Dependiente)
+                .Include(t => t.Producto)
+                .ToList();
+            var trasladosVentas = _db.Set<TrasladoVenta>()
+                .Include(t => t.Destino)
+                .Include(t => t.Turno.Bar)
+                .Include(t => t.Turno.Dependiente)
+                .Include(t => t.Producto)
+                .ToList();
+            if (User.IsInRole(DefinicionRoles.Dependiente))
+            {
+                var dependiente = _db.Set<Dependiente>().SingleOrDefault(u => u.Usuario.UserName == User.Identity.Name);
+                if (_db.Set<Turno>().Any(t => t.DependienteId == dependiente.Id && t.Activo))
+                {
+                    var turno = _db.Set<Turno>().SingleOrDefault(t => t.DependienteId == dependiente.Id && t.Activo);
+                    traslados = traslados.Where(t => t.DestinoId == turno.BarId && t.Fecha >= turno.FechaInicio).ToList();
+                    trasladosVentas = trasladosVentas.Where(t => t.DestinoId == turno.BarId && t.Fecha >= turno.FechaInicio).ToList();
+                    if (traslados.Any())
+                    {
+                        notificaciones.Add(new NotificacionVM { Descripcion = $"Existen {traslados.Count } traslados pendientes por recibir.", Url = "/trasladosrecibidos" });
+                    }
+                    if (trasladosVentas.Any())
+                    {
+                        notificaciones.Add(new NotificacionVM { Descripcion = $"Existen {trasladosVentas.Count } traslados de venta pendientes por recibir.", Url = "/trasladosrecibidos/deventas" });
+                    }
+                }
+            }
+            return View(notificaciones);
+        }
+    }
+}
---
+++ GestionBares/ViewModels/NotificacionVM.cs
@@ -0,0 +1,8 @@
+namespace GestionBares.ViewModels
+{
+    public class NotificacionVM
+    {
+        public string Descripcion { get; set; }
+        public string Url { get; set; }
+    }
+}
